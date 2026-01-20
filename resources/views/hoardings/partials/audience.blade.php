06bd3811f7b4efbada1257f56e3f066956d51182
Sweety Singh <dbm.laraveldeveloper3@gmail.com>
2026-01-20 11:02:51 +0530

    audience.blade.php and getAudinece attrivute setted

--- resources/views/hoardings/partials/audience.blade.php
+++ resources/views/hoardings/partials/audience.blade.php
@@ -1,13 +1,12 @@
 <div>
     <h3 class="text-sm font-semibold mb-3">Audience Type</h3>
-
     <div class="flex flex-wrap gap-3 text-sm text-gray-700">
-        @foreach(($hoarding->audience_types ?? []) as $audience)
+        @forelse($hoarding->audience_types as $audience)
             <span class="flex items-center gap-1">
                 ✔ {{ $audience }}
             </span>
-        @endforeach
+        @empty
+            <span class="text-gray-400 text-sm">No audience data available</span>
+        @endforelse
     </div>
 </div>
-
-<hr class="my-6 border-gray-300">
