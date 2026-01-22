f91d6cc61d170fd7894cdf9628f115d49937d104
Sweety Singh <dbm.laraveldeveloper3@gmail.com>
2026-01-22 17:34:21 +0530

    monthly_price issue resolved

--- database/migrations/2026_01_22_101547_change_columns_to_nullable_in_hoardings_table.php
+++ database/migrations/2026_01_22_101547_change_columns_to_nullable_in_hoardings_table.php
@@ -13,17 +13,17 @@ return new class extends Migration
     {
         Schema::table('hoardings', function (Blueprint $table) {
            // Price & Commission
-            $table->decimal('base_monthly_price', 12, 2)->nullable()->change();
-            $table->decimal('monthly_price', 12, 2)->nullable()->change();
-            $table->decimal('commission_percent', 5, 2)->nullable()->change();
+            // $table->decimal('base_monthly_price', 12, 2)->nullable()->change();
+            // $table->decimal('monthly_price', 12, 2)->nullable()->change();
+            // $table->decimal('commission_percent', 5, 2)->nullable()->change();
 
             // Integers
             $table->unsignedInteger('grace_period_days')->nullable()->change();
             $table->unsignedInteger('min_booking_duration')->nullable()->change();
 
             // Booleans
-            $table->boolean('nagar_nigam_approved')->nullable()->change();
-            $table->boolean('geolocation_verified')->nullable()->change();
+            // $table->boolean('nagar_nigam_approved')->nullable()->change();
+            // $table->boolean('geolocation_verified')->nullable()->change();
 
             // Strings
             $table->string('state')->nullable()->change();
@@ -36,13 +36,13 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('hoardings', function (Blueprint $table) {
-          $table->decimal('base_monthly_price', 12, 2)->default(0)->change();
-            $table->decimal('monthly_price', 12, 2)->default(0)->change();
-            $table->decimal('commission_percent', 5, 2)->default(0)->change();
-            $table->unsignedInteger('grace_period_days')->default(0)->change();
+        //   $table->decimal('base_monthly_price', 12, 2)->default(0)->change();
+        //     $table->decimal('monthly_price', 12, 2)->default(0)->change();
+        //     $table->decimal('commission_percent', 5, 2)->default(0)->change();
+        //     $table->unsignedInteger('grace_period_days')->default(0)->change();
             $table->unsignedInteger('min_booking_duration')->default(1)->change();
-            $table->boolean('nagar_nigam_approved')->default(false)->change();
-            $table->boolean('geolocation_verified')->default(false)->change();
+        //     $table->boolean('nagar_nigam_approved')->default(false)->change();
+        //     $table->boolean('geolocation_verified')->default(false)->change();
             $table->string('state')->default('India')->change();
         });
     }
