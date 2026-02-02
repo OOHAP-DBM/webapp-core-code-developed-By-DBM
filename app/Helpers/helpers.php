f82139b1898f279e118acf9874dc510ec5b6c398
Sweety Singh <dbm.laraveldeveloper3@gmail.com>
2026-02-02 11:42:27 +0530

    modified shortlist api , sending images, percentage

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -10,4 +10,29 @@
         return $currency . number_format($price, 0);
     }
 
+     function calculateOffPercentage(float $basePrice, float $finalPrice): int
+    {
+        if ($basePrice <= 0 || $finalPrice >= $basePrice) {
+            return 0;
+        }
+
+        $discount = (($basePrice - $finalPrice) / $basePrice) * 100;
+        
+        return (int) round($discount);
+    }
+
+    /**
+     * Calculate the final price after applying a percentage discount.
+     */
+     function calculateDiscountedPrice(float $basePrice, float $discountPercent): float
+    {
+        if ($discountPercent <= 0) {
+            return round($basePrice, 2);
+        }
+
+        $discountAmount = ($basePrice * $discountPercent) / 100;
+        $finalPrice = $basePrice - $discountAmount;
+
+        return round($finalPrice, 2);
+    }
 
