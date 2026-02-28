98696699ca8f9566078fba290e65bc884f98e2f4
khushboo <laravel1.dbm@gmail.com>
2026-02-28 11:58:42 +0530

    notification

--- app/Services/FcmService.php
+++ app/Services/FcmService.php
@@ -2,52 +2,45 @@
 
 namespace App\Services;
 
-use Google\Auth\Credentials\ServiceAccountCredentials;
-use Illuminate\Support\Facades\Http;
+use Kreait\Firebase\Contract\Messaging;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\Notification;
 
 class FcmService
 {
-    protected function getAccessToken()
+    protected Messaging $messaging;
+
+    public function __construct(Messaging $messaging)
     {
-        $credentialsPath = base_path(config('app.firebase_credentials'));
-
-        $credentials = new ServiceAccountCredentials(
-            ['https://www.googleapis.com/auth/firebase.messaging'],
-            $credentialsPath
-        );
-
-        $token = $credentials->fetchAuthToken();
-
-        return $token['access_token'] ?? null;
+        $this->messaging = $messaging;
     }
 
-    public function send($fcmToken, $title, $body, $data = [])
+    public function sendToToken(string $token, string $title, string $body, array $data = [])
     {
-        if (!$fcmToken) {
-            return false;
-        }
+        $message = CloudMessage::new()
+            ->toToken($token)
+            ->withNotification(Notification::create($title, $body))
+            ->withData($data);
 
-        $credentialsPath = base_path(config('app.firebase_credentials'));
-        $projectId = json_decode(file_get_contents($credentialsPath))->project_id;
+        return $this->messaging->send($message);
+    }
 
-        $accessToken = $this->getAccessToken();
+    public function sendToMultiple(array $tokens, string $title, string $body, array $data = [])
+    {
+        $message = CloudMessage::new()
+            ->withNotification(Notification::create($title, $body))
+            ->withData($data);
 
-        if (!$accessToken) {
-            return false;
-        }
+        return $this->messaging->sendMulticast($message, $tokens);
+    }
 
-        $response = Http::withToken($accessToken)
-            ->post("https://fcm.googleapis.com/v1/projects/{$projectId}/messages:send", [
-                "message" => [
-                    "token" => $fcmToken,
-                    "notification" => [
-                        "title" => $title,
-                        "body"  => $body,
-                    ],
-                    "data" => $data,
-                ]
-            ]);
+    public function sendToTopic(string $topic, string $title, string $body, array $data = [])
+    {
+        $message = CloudMessage::new()
+            ->toTopic($topic)
+            ->withNotification(Notification::create($title, $body))
+            ->withData($data);
 
-        return $response->successful();
+        return $this->messaging->send($message);
     }
 }
