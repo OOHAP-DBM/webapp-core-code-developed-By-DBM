169e5fda05f04a74c211234959e65c5c35aadab8
Sweety Singh <dbm.laraveldeveloper3@gmail.com>
2026-03-12 13:04:46 +0530

    app/Services/Sms/TwilioGateway.php name issue resolved

--- app/Services/Sms/TwilioGateway.php
+++ app/Services/Sms/TwilioGateway.php
@@ -5,15 +5,15 @@ namespace App\Services\Sms;
 use Illuminate\Support\Facades\Http;
 use App\Models\Setting;
 
-class Msg91Gateway implements SmsGatewayInterface
+class TwilioGateway implements SmsGatewayInterface
 {
     public function send($mobile, $message)
     {
-        $authKey = Setting::get('sms_msg91_auth_key');
-        $sender  = Setting::get('sms_msg91_sender_id');
-        $route   = Setting::get('sms_msg91_route', 4);
-        $country = Setting::get('sms_msg91_country', 91);
-        $apiUrl  = Setting::get('sms_msg91_api_url');
+        $authKey = Setting::get('sms_twilio_auth_key');
+        $sender  = Setting::get('sms_twilio_sender_id');
+        $route   = Setting::get('sms_twilio_route', 4);
+        $country = Setting::get('sms_twilio_country', 91);
+        $apiUrl  = Setting::get('sms_twilio_api_url');
 
         $response = Http::withHeaders([
             'authkey' => $authKey
