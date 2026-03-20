4d64618cc1f9ebb43614e9c07c5e144d45056fcc
dbm.laraveldeveloper <dbm.laraveldeveloper@gmail.com>
2026-03-20 18:28:33 +0530

    only fix spacing

--- resources/views/vendor/offers/create.blade.php
+++ resources/views/vendor/offers/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Create Offer')
 @section('content')
-<div class="px-6 py-6 bg-gray-50">
+<div class=" bg-gray-50">
     <div class="grid grid-cols-1 lg:grid-cols-12 gap-6">
         <div class="lg:col-span-7">
             @include('vendor.offers.components.offer-form')
